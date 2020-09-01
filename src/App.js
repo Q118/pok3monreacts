b1eed8db69a950971794ed320b49b0f1319acf80
Q118 <shelbyfish91@gmail.com>
2020-09-01 11:06:57 -0700

    axios call to json data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
+//axios in order to call api
 
 function App() {
   //set up the initial/current state, starting empty in this case. then the method to update it
   //default state on the right of =, will show up on first render
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState([])
+
+//this line will make a fetch request and return us a promise
+//the promise will have the response for our request variable that gets returned
+  //the property of 'data' will hold all the json data from the api
+axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
+  //map through to get name from each one 
+  //set pokemon variable to that value
+setPokemon(res.data.results.map(p => p.name))
+})
+
+
+
   return (
    // pass pokemon down to our pokemon list
    <PokemonList pokemon={pokemon} />

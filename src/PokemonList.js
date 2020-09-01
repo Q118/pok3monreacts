a105c1da9adc023ad99b4d7216011f6e59314c59
Q118 <shelbyfish91@gmail.com>
2020-09-01 10:56:39 -0700

    setting up the list component initial state

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function PokemonList() {
+
+//pass in pokemon as props
+export default function PokemonList( { pokemon } ) {
     return (
         <div>
-            
+            {/* everything inside of () will be rendered by the below function */}
+            {pokemon.map(p => (
+                <div key={p}>{p}</div>
+            ))}
         </div>
     )
 }
+
+
+
+//gotta use a unique key anytime inside of a loop.. we use p since each poke name is unique
